1fe501919775494eaab8c0ceed9e7f9d5973fa7e
confilife87-SCNB <confilife87@gmail.com>
2026-04-25 20:06:34 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // js/config.js
 const CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbzxkOEWGZzkZ1m7Jpo28FcJmg_lfH0UeuonnkMIfnw6NUOR2oQCUHh0scG-eDvQBoWblQ/exec',
-  VERSION: '4.1-pwa'
+  SUPABASE_URL: 'https://dfueoyfgqmumtceseahy.supabase.co',
+  VERSION: '5.0-supabase'
 };
