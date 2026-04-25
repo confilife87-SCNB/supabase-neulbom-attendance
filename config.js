b3275e4026bb077f88414244118f04aa417a3992
confilife87-SCNB <confilife87@gmail.com>
2026-04-25 20:00:26 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// js/config.js
+const CONFIG = {
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbzxkOEWGZzkZ1m7Jpo28FcJmg_lfH0UeuonnkMIfnw6NUOR2oQCUHh0scG-eDvQBoWblQ/exec',
+  VERSION: '4.1-pwa'
+};
