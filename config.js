dfaac3b1cf4c25fcb1032585b231cb8fb766ed51
confilife87-SCNB <confilife87@gmail.com>
2026-05-22 14:51:06 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 const CONFIG = {
   SUPABASE_URL: 'https://dfueoyfgqmumtceseahy.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_g7WziIVZkXPV6eGM-5zUTA_0PQP60kz',
-  VERSION: '5.1-supabase'
+  VERSION: '2026.05.22.0000'
 };
